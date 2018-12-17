e41ab0a3d008bd25c014860e236b1e309eef221e
wildlifehexagon <crossthebreeze@gmail.com>
2018-12-17 13:33:12 -0600

    feat: add repository interface

---
+++ internal/repository/repository.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"github.com/dictyBase/go-genproto/dictybaseapis/stock"
+	"github.com/dictyBase/modware-stock/internal/model"
+)
+
+// StockRepository is an interface for accessing stock information
+type StockRepository interface {
+	GetStock(id string) (*model.StockDoc, error)
+	AddStock(no *stock.NewStock) (*model.StockDoc, error)
+	EditStock(uo *stock.StockUpdate) (*model.StockDoc, error)
+	ListStocks(cursor int64, limit int64) ([]*model.StockDoc, error)
+	ListStrains(cursor int64, limit int64) ([]*model.StockDoc, error)
+	ListPlasmids(cursor int64, limit int64) ([]*model.StockDoc, error)
+	RemoveStock(id string) error
+	ClearStocks() error
+}
