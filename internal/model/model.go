02e0189c9ed3dbc3b01125e1e53aa49890458428
wildlifehexagon <crossthebreeze@gmail.com>
2018-12-17 13:30:14 -0600

    feat: add StockDoc model

---
+++ internal/model/model.go
@@ -0,0 +1,32 @@
+package model
+
+import (
+	"time"
+
+	driver "github.com/arangodb/go-driver"
+)
+
+// StockDoc is the data structure for stock orders
+type StockDoc struct {
+	driver.DocumentMeta
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+	CreatedBy       string    `json:"created_by"`
+	UpdatedBy       string    `json:"updated_by"`
+	Summary         string    `json:"summary"`
+	EditableSummary string    `json:"editable_summary"`
+	Depositor       string    `json:"depositor"`
+	Genes           []string  `json:"genes"`
+	Dbxrefs         []string  `json:"dbxrefs"`
+	Publications    []string  `json:"publications"`
+	SystematicName  string    `json:"systematic_name"`
+	Descriptor      string    `json:"descriptor"`
+	Species         string    `json:"species"`
+	Plasmid         string    `json:"plasmid"`
+	Parents         []string  `json:"parents"`
+	Names           []string  `json:"names"`
+	ImageMap        string    `json:"image_map"`
+	Sequence        string    `json:"sequence"`
+	Keywords        []string  `json:"keywords"`
+	NotFound        bool
+}
