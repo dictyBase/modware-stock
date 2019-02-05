df98de53cb8e46f2e056dc0567cf59dae00c24f8
wildlifehexagon <crossthebreeze@gmail.com>
2019-02-05 09:37:23 -0600

    refactor: update Get AQL statements to use proper edge collection type filtering

--- internal/repository/arangodb/arangodb.go
+++ internal/repository/arangodb/arangodb.go
@@ -149,10 +149,9 @@ func NewStockRepo(connP *manager.ConnectParams, collP *CollectionParams) (reposi
 func (ar *arangorepository) GetStrain(id string) (*model.StockDoc, error) {
 	m := &model.StockDoc{}
 	bindVars := map[string]interface{}{
-		"@stock_collection":      ar.stock.Name(),
-		"@stock_type_collection": ar.stockType.Name(),
-		"id":                     id,
-		"graph":                  ar.stockPropType.Name(),
+		"@stock_collection": ar.stock.Name(),
+		"id":                id,
+		"graph":             ar.stockPropType.Name(),
 	}
 	r, err := ar.database.GetRow(statement.StockGetStrain, bindVars)
 	if err != nil {
@@ -172,10 +171,9 @@ func (ar *arangorepository) GetStrain(id string) (*model.StockDoc, error) {
 func (ar *arangorepository) GetPlasmid(id string) (*model.StockDoc, error) {
 	m := &model.StockDoc{}
 	bindVars := map[string]interface{}{
-		"@stock_collection":      ar.stock.Name(),
-		"@stock_type_collection": ar.stockType.Name(),
-		"id":                     id,
-		"graph":                  ar.stockPropType.Name(),
+		"@stock_collection": ar.stock.Name(),
+		"id":                id,
+		"graph":             ar.stockPropType.Name(),
 	}
 	r, err := ar.database.GetRow(statement.StockGetPlasmid, bindVars)
 	if err != nil {
