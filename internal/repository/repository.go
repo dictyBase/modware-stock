879490e1859fa1b1b5c7fd8cfe8c30a747692bf5
wildlifehexagon <crossthebreeze@gmail.com>
2019-02-05 09:40:04 -0600

    refactor: remove the old list methods and statements

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -13,9 +13,6 @@ type StockRepository interface {
 	AddPlasmid(ns *stock.NewStock) (*model.StockDoc, error)
 	EditPlasmid(us *stock.StockUpdate) (*model.StockDoc, error)
 	EditStrain(us *stock.StockUpdate) (*model.StockDoc, error)
-	ListStocks(cursor int64, limit int64) ([]*model.StockDoc, error)
-	ListStrains(cursor int64, limit int64) ([]*model.StockDoc, error)
-	ListPlasmids(cursor int64, limit int64) ([]*model.StockDoc, error)
 	RemoveStock(id string) error
 	ClearStocks() error
 }
