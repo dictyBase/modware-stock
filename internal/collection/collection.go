7b28395037bb76b9cddaeefa1829ecd2472e0a61
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-09-03 15:06:48 -0500

    feat: add FilterFlags function for flag slice

--- internal/collection/collection.go
+++ internal/collection/collection.go
@@ -1,6 +1,10 @@
 package collection
 
-// Map applies the given function to each element of a, returning slice of
+import (
+	"github.com/urfave/cli"
+)
+
+// MapString applies the given function to each element of string a, returning string slice of
 // results
 func MapString(a []string, fn func(string) string) []string {
 	if len(a) == 0 {
@@ -11,4 +15,20 @@ func MapString(a []string, fn func(string) string) []string {
 		sl[i] = fn(v)
 	}
 	return sl
+}
+
+// FilterFlags returns a new slice of cli Flags that
+// passes the test implemented by the given functions
+func FilterFlags(af []cli.Flag, fn func(cli.Flag) bool) []cli.Flag {
+	if len(af) == 0 {
+		return f
+	}
+	var nf []cli.Flag
+	for _, f := range af {
+		if !fn(f) {
+			continue
+		}
+		nf = append(nf, f)
+	}
+	return nf
 }
