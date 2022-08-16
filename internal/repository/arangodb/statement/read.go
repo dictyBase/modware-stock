eefb434f3cc70b0a9cb20f936fef733518366d64
Siddhartha Basu <biosidd@gmail.com>
2022-08-16 18:24:33 -0500

    refactor: add ontology constraint to filtering of strains

--- internal/repository/arangodb/statement/read.go
+++ internal/repository/arangodb/statement/read.go
@@ -62,20 +62,13 @@ const (
 	`
 	StrainListFromIds = `
 		FOR id IN @ids
-			FOR v, e IN 1..1 OUTBOUND 
-								CONCAT(@stock_collection,"/",id) 
-								GRAPH @prop_graph
+			FOR v, e IN 1..1 OUTBOUND CONCAT(@stock_collection,"/",id) GRAPH @prop_graph
 				LET parent = (
-					FOR p IN 1..1 INBOUND 
-								CONCAT(@stock_collection,"/",id) 
-								GRAPH @parent_graph
+					FOR p IN 1..1 INBOUND CONCAT(@stock_collection,"/",id) GRAPH @parent_graph
 						RETURN p.stock_id
-								
 				)
 				LET term = (
-					FOR cg IN 1..1 OUTBOUND 
-									CONCAT(@stock_collection,"/",id) 
-									GRAPH @stock_cvterm_graph
+					FOR cg IN 1..1 OUTBOUND CONCAT(@stock_collection,"/",id) GRAPH @stock_cvterm_graph
 						FOR cv IN @@cv_collection
 							FILTER cg.deprecated == false
 							FILTER cg.graph_id == cv._id
@@ -97,31 +90,53 @@ const (
 						}
 					})
 	`
-	StrainList = `
-		FOR s IN %s
-			FOR v, e IN 1..1 OUTBOUND s GRAPH '%s'
-				FILTER e.type == 'strain'
-				SORT s.created_at DESC
-				LIMIT %d
-				RETURN MERGE(
-					s,
-					{
-						strain_properties: { 
-							label: v.label, 
-							species: v.species, 
-							plasmid: v.plasmid, 
-							names: v.names
-						} 
-					}
-				)
-	`
 	StrainListFilter = `
-		FOR s IN %s
-			FOR v, e IN 1..1 OUTBOUND s GRAPH '%s'
+		FOR cvterm in @@cvterm_collection
+			FOR cv IN @@cv_collection
+				FOR stock IN 1..1 INBOUND cvterm GRAPH @stock_cvterm_graph
+					FOR prop,etype IN 1..1 OUTBOUND stock GRAPH @stockprop_graph
+						FILTER cvterm.graph_id == cv._id
+						FILTER etype.type == 'strain'
+						@filter
+						SORT stock.created_at DESC
+						LIMIT @limit
+						RETURN MERGE(stock,{
+								strain_properties: { 
+									label: prop.label, 
+									species: prop.species, 
+									plasmid: prop.plasmid, 
+									names: prop.names
+								} 
+							}
+						)
+	`
+	StrainListFilterWithCursor = `
+		FOR cvterm in @@cvterm_collection
+			FOR cv IN @@cv_collection
+				FOR stock IN 1..1 INBOUND cvterm GRAPH @stockonto_graph
+					FOR prop,etype IN 1..1 OUTBOUND stock GRAPH @stockprop_graph
+						FILTER cvterm.graph_id == cv._id
+						FILTER etype.type == 'strain'
+						@filter
+						FILTER stock.created_at <= DATE_ISO8601(@cursor)
+						SORT stock.created_at DESC
+						LIMIT @limit
+						RETURN MERGE(stock,{
+								strain_properties: { 
+									label: prop.label, 
+									species: prop.species, 
+									plasmid: prop.plasmid, 
+									names: prop.names
+								} 
+							}
+						)
+	`
+	StrainList = `
+		FOR s IN @@stock_collection
+			FOR v, e IN 1..1 OUTBOUND s GRAPH @graph
 				FILTER e.type == 'strain'
-				%s
 				SORT s.created_at DESC
-				LIMIT %d
+				LIMIT @limit
 				RETURN MERGE(
 					s,
 					{
@@ -135,32 +150,12 @@ const (
 				)
 	`
 	StrainListWithCursor = `
-		FOR s IN %s
-			FOR v, e IN 1..1 OUTBOUND s GRAPH '%s'
+		FOR s IN @@stock_collection
+			FOR v, e IN 1..1 OUTBOUND s GRAPH @graph
 				FILTER e.type == 'strain'
-				FILTER s.created_at <= DATE_ISO8601(%d)
+				FILTER s.created_at <= DATE_ISO8601(@cursor)
 				SORT s.created_at DESC
-				LIMIT %d
-				RETURN MERGE(
-					s,
-					{
-						strain_properties: { 
-							label: v.label, 
-							species: v.species, 
-							plasmid: v.plasmid, 
-							names: v.names
-						} 
-					}
-				)
-	`
-	StrainListFilterWithCursor = `
-		FOR s IN %s
-			FOR v, e IN 1..1 OUTBOUND s GRAPH '%s'
-				FILTER e.type == 'strain'
-				%s
-				FILTER s.created_at <= DATE_ISO8601(%d)
-				SORT s.created_at DESC
-				LIMIT %d
+				LIMIT @limit
 				RETURN MERGE(
 					s,
 					{
