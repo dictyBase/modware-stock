a97b274ea279ef5956d8e819f7f4ae4bb108f275
Siddhartha Basu <biosidd@gmail.com>
2022-08-23 16:36:17 -0500

    feat: add ontology and term name mappings

--- internal/repository/arangodb/fields.go
+++ internal/repository/arangodb/fields.go
@@ -2,15 +2,18 @@ package arangodb
 
 // FMap maps filters to database fields
 var FMap = map[string]string{
-	"created_at":   "created_at",
-	"updated_at":   "updated_at",
-	"depositor":    "depositor",
-	"summary":      "summary",
-	"plasmid":      "plasmid",
-	"species":      "species",
-	"name":         "names",
+	"created_at":   "s.created_at",
+	"updated_at":   "s.updated_at",
+	"depositor":    "s.depositor",
+	"summary":      "s.summary",
+	"id":           "s.stock_id",
+	"gene":         "s.genes",
+	"plasmid":      "stock_prop.plasmid",
+	"species":      "stock_prop.species",
+	"name":         "stock_prop.names",
+	"label":        "stock_prop.label",
+	"ontology":     "cv.metadata.namespace",
+	"tag":          "cvterm.label",
 	"parent":       "parent",
 	"plasmid_name": "name",
-	"id":           "stock_id",
-	"label":        "label",
 }
