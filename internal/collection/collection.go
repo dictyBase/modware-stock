4d8ba5594a022476cd066ca355c536a9a0138c10
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-10-28 09:41:30 -0500

    refactor(collection): optimize Map function by pre-allocating slice capacity
    Pre-allocating the slice with the exact length of the input slice avoids
    repeated memory reallocations that occur with `append` in a loop,
    leading to better performance, especially for large slices.

--- internal/collection/collection.go
+++ internal/collection/collection.go
@@ -7,9 +7,9 @@ import (
 // Map applies the given function to each element of slice a, returning slice of
 // results
 func Map[T any, M any](a []T, fn func(T) M) []M {
-	anySlice := make([]M, 0)
-	for _, elem := range a {
-		anySlice = append(anySlice, fn(elem))
+	anySlice := make([]M, len(a))
+	for i, elem := range a {
+		anySlice[i] = fn(elem)
 	}
 
 	return anySlice
