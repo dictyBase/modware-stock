06ecde9605f0ab413543873bac0e57d03acc6c2b
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-10-31 12:01:38 -0500

    refactor(repository): update StockRepository interface methods to return IOEither for error handling
    Update the return types of `AddPlasmid`, `EditPlasmid`, and
    `LoadPlasmid` methods in the `StockRepository` interface to use
    `IOE.IOEither[error, *model.StockDoc]`. This change standardizes error
    handling across plasmid-related operations to use the `IOEither` type,
    promoting consistent functional error management within the repository
    layer.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -15,14 +15,14 @@ type StockRepository interface {
 	GetStrain(id string) (*model.StockDoc, error)
 	GetPlasmid(id string) IOE.IOEither[error, *model.StockDoc]
 	AddStrain(ns *stock.NewStrain) (*model.StockDoc, error)
-	AddPlasmid(ns *stock.NewPlasmid) (*model.StockDoc, error)
+	AddPlasmid(ns *stock.NewPlasmid) IOE.IOEither[error, *model.StockDoc]
 	EditStrain(us *stock.StrainUpdate) (*model.StockDoc, error)
-	EditPlasmid(us *stock.PlasmidUpdate) (*model.StockDoc, error)
+	EditPlasmid(us *stock.PlasmidUpdate) IOE.IOEither[error, *model.StockDoc]
 	ListStrains(s *stock.StockParameters) ([]*model.StockDoc, error)
 	ListStrainsByIDs(s *stock.StockIdList) ([]*model.StockDoc, error)
 	ListPlasmids(s *stock.StockParameters) IOE.IOEither[error, []*model.StockDoc]
 	LoadStrain(id string, es *stock.ExistingStrain) (*model.StockDoc, error)
-	LoadPlasmid(id string, ep *stock.ExistingPlasmid) (*model.StockDoc, error)
+	LoadPlasmid(id string, ep *stock.ExistingPlasmid) IOE.IOEither[error, *model.StockDoc]
 	RemoveStock(id string) error
 	Dbh() *manager.Database
 	LoadOboJSON(r io.Reader) (*storage.UploadInformation, error)
